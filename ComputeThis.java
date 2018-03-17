440795820a88f5ca18b307b3a608ab0e41bc57ea
Alex Yang <a.yang2@share.epsb.ca>
2018-03-17 16:32:34 +0000

    Calculated d2

--- ComputeThis.java
+++ ComputeThis.java
@@ -8,8 +8,10 @@ public class ComputeThis
 	double ang2 = Math.toRadians(122);
 
 	double d1 = 3 * Math.PI * Math.sin(ang1) + Math.abs(Math.cos(ang2)); 
+	double d2 = Math.pow(14.72, 3.801) + Math.log(72); 
 
-	System.out.println("d1 =" + d1);
+	System.out.println("d1 = " + d1);
+	System.out.println("d2 = " + d2);
 
 	}
 }
